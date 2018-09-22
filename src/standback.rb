8d0dae204aa9e84c726a67d523690a8c28d93bf7
akouryy <akouryy7@gmail.com>
2018-09-22 17:50:14 +0900

    Remove newline after output

--- src/standback.rb
+++ src/standback.rb
@@ -2,4 +2,4 @@ require_relative 'parser'
 require_relative 'runner'
 
 f = File.read ARGV.shift
-puts Standback::Runner.run Standback::Parser::parse(f), gets(nil) || ''
+print Standback::Runner.run Standback::Parser::parse(f), gets(nil) || ''
